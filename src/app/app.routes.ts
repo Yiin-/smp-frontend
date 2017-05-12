bf56304f06288ab7c2b442010b31fb7b06ec93e9
Stanislovas Janonis <stanislovas.janonis@gmail.com>
2017-05-12 04:47:21 +0300

    Register `google` component route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { CanActivateSignIn } from './sign-in/can-activate.permission';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoogleComponent } from './google/google.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthGuardService } from './auth/auth-guard.service';
 
@@ -21,6 +22,11 @@ const AppRoutes: Routes = [
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
+    },
+    {
+        path: 'google',
+        component: GoogleComponent,
+        canActivate: [AuthGuardService]
     },
     { path: '**', component: PageNotFoundComponent }
 ];
